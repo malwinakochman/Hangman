45c7c6f08bf5aa8bc8d93168bea39fca88f2494c
malwinakochman <s223011@student.uek.krakow.pl>
2022-09-02 23:39:09 +0200

    2

--- hangman.py
+++ hangman.py
@@ -2,34 +2,40 @@ import requests
 import random
 import string
 
-#word_site = "https://www.mit.edu/~ecprice/wordlist.10000"
-#response = requests.get(word_site)
-#words = response.content.splitlines()
-#word = random.choice(words)
+word_site = "https://www.mit.edu/~ecprice/wordlist.10000"
+response = requests.get(word_site)
+words = response.content.splitlines()
+word = str(random.choice(words))
 haslo = []
-word2 = "zmiana"
 
-for i in range(0, len(word2)):
-    haslo.append("_")
+for l in word:
+    if l == " ":
+        haslo.append(" ")
+    else:
+        haslo.append("_")
 
 class Game():
 
     def __init__(self):
         self.used_letters = ""
-        self.counter = 0
 
     def letter_guess(self):
         letter = input("Please enter letter\n")
+        
+        #Sprawdzamy czy jest literą czy znakiem
+        counter = 0
+        for p in string.ascii_letters:
+            if letter == p:
+                    counter += 1
+        if counter == 0:
+            print("It is sign. Please enter letter.")
+            letter = input("Please enter letter\n")
+
+        #Sprawdzamy czy litera juz byla uzyta
         for y in self.used_letters:
             if y == letter:
                 print("The letter was used. Please enter another letter.")
-                
-
-        for p in string.ascii_letters:
-            if letter != p:
-                    self.counter += 1
-        if self.counter == 0:
-            print("It is sign. Please enter letter.")
+                letter = input("Please enter letter\n")
 
         self.used_letters += letter
         counter = 0
@@ -38,26 +44,36 @@ class Game():
         chwilowe = 0
 
         #ile razy wystepuje sprawdzamy
-        for m in word2:
-            if letter == m:
+        for m in word:
+            if letter.lower() == m.lower():
                 counter3 +=1
 
         #wpisujemy do tablicy litery
-        
         while counter4 != counter3:
-            for n in word2:
+            for n in word:
                 counter += 1
-                if letter == n:
+                if letter.lower() == n.lower():
                     counter4 += 1
-                    haslo[counter+chwilowe-1] = letter
+                    haslo[counter+chwilowe-1] = letter.lower()
                     chwilowe += counter
                     counter = 0
         
+                    
         print(*haslo)
+        
                  
 
 game = Game()
 for tries in range(0,10):
-    game.letter_guess()
+    haslokoncowe = ""
+    for u in haslo:
+        haslokoncowe += u
+    if haslokoncowe == word:    
+            print(f"You won the game. The word is {word}")
+            break
+    else:
+        game.letter_guess()
+
+print(f"All tries have been used. Game over. The word is {word}")
 
 
