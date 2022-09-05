2fba95090897722ef80fa56045a60db5ef7b9280
malwinakochman <s223011@student.uek.krakow.pl>
2022-09-05 16:34:39 +0200

    New names of variables, new functions

--- hangman.py
+++ hangman.py
@@ -6,14 +6,14 @@ word_site = "https://www.mit.edu/~ecprice/wordlist.10000"
 response = requests.get(word_site)
 words = response.content.splitlines()
 word = str(random.choice(words))
-haslo = []
+sentence = []
 
-#Wpisujemy luki w tabelke
-for l in word:
-    if l == " ":
-        haslo.append(" ")
+#Entering gaps in the array
+for letter_of_word in word:
+    if letter_of_word == " ":
+        sentence.append(" ")
     else:
-        haslo.append("_")
+        sentence.append("_")
 
 class Game():
 
@@ -23,7 +23,7 @@ class Game():
     def letter_guess(self):
         letter = input("Please enter letter\n")
         
-        #Sprawdzamy czy jest literą czy znakiem
+        #Checking whether it is a letter or a sign
         counter = 0
         for p in string.ascii_letters:
             if letter == p:
@@ -32,7 +32,7 @@ class Game():
             print("It is sign. Please enter letter.")
             letter = input("Please enter letter\n")
 
-        #Sprawdzamy czy litera juz byla uzyta
+        #Checking if the letter has already been used
         for y in self.used_letters:
             if y == letter:
                 print("The letter was used. Please enter another letter.")
@@ -40,36 +40,36 @@ class Game():
 
         self.used_letters += letter
         counter = 0
+        counter2 = 0
         counter3 = 0
-        counter4 = 0
         chwilowe = 0
 
-        #ile razy wystepuje sprawdzamy
+        #Checking how many times it occurs
         for m in word:
             if letter.lower() == m.lower():
-                counter3 +=1
+                counter2 +=1
 
-        #wpisujemy do tablicy litery
-        while counter4 != counter3:
+        #Writing letters into the array
+        while counter3 != counter2:
             for n in word:
                 counter += 1
                 if letter.lower() == n.lower():
-                    counter4 += 1
-                    haslo[counter+chwilowe-1] = letter.lower()
+                    counter3 += 1
+                    sentence[counter+chwilowe-1] = letter.lower()
                     chwilowe += counter
                     counter = 0
         
                     
-        print(*haslo)
+        print(*sentence)
         
                  
 
 game = Game()
 for tries in range(0,10):
-    haslokoncowe = ""
-    for u in haslo:
-        haslokoncowe += u
-    if haslokoncowe == word:    
+    final_sentence = ""
+    for letter_of_sentence in sentence:
+        final_sentence += u
+    if final_sentence == word:    
             print(f"You won the game. The word is {word}")
             break
     else:
