ea3d65c33c98cc5f2a895bba2378fa8988b73a6e
malwinakochman <s223011@student.uek.krakow.pl>
2022-09-05 16:35:30 +0200

    New names of variables, new functions

--- hangman.py
+++ hangman.py
@@ -1,80 +1,96 @@
 import requests
 import random
-import string
 
 word_site = "https://www.mit.edu/~ecprice/wordlist.10000"
 response = requests.get(word_site)
 words = response.content.splitlines()
-word = str(random.choice(words))
-sentence = []
+#word = str(random.choice(words))
+WORD = "zamiana"
+BLANK_SYMBOL = "_"
+WORDS_DELIMITER = " "
+LETTER_INPUT_MESSAGE = "Please enter letter\n"
+NOT_LETTER_MESSAGE = "It is sign."
+USED_LETTER_MESSAGE = "The letter was used."
+MAX_NUMBER_OF_TRIES = 10
+VICTORY_MESSAGE = f"You won the game. The word is {WORD}"
+GAME_OVER_MESSAGE = f"All tries have been used. Game over. The word is {WORD}"
 
-#Entering gaps in the array
-for letter_of_word in word:
-    if letter_of_word == " ":
-        sentence.append(" ")
-    else:
-        sentence.append("_")
-
-class Game():
+class Hangman():
 
     def __init__(self):
         self.used_letters = ""
+        self.letter = ""
+        self.sentence = []
 
-    def letter_guess(self):
-        letter = input("Please enter letter\n")
+    def request_an_input(self):
+        self.letter = input(LETTER_INPUT_MESSAGE)
+        self.request_an_input_if_not_letter()
+        self.request_an_input_if_letter_was_used()
+        self.used_letters += self.letter
+
+    def generate_blanks(self, word):
+        sentence = []
+        for letter_of_word in word:
+            if letter_of_word == WORDS_DELIMITER:
+                sentence.append(self.letter)
+            else:
+                sentence.append(BLANK_SYMBOL)
+        return sentence
         
-        #Checking whether it is a letter or a sign
+    def request_an_input_if_not_letter(self):
+        if not self.letter.isalpha():
+            print(NOT_LETTER_MESSAGE)
+            self.request_an_input()
+           
+    def request_an_input_if_letter_was_used(self):
+        for used_letter in self.used_letters:
+            if used_letter == self.letter:
+                print(USED_LETTER_MESSAGE)
+                self.request_an_input()
+
+    def count_letter_occurences(self, word):
         counter = 0
-        for p in string.ascii_letters:
-            if letter == p:
-                    counter += 1
-        if counter == 0:
-            print("It is sign. Please enter letter.")
-            letter = input("Please enter letter\n")
+        for letter_of_word in word:
+            if self.letter.lower() == letter_of_word.lower():
+                counter +=1
+        return counter
 
-        #Checking if the letter has already been used
-        for y in self.used_letters:
-            if y == letter:
-                print("The letter was used. Please enter another letter.")
-                letter = input("Please enter letter\n")
+    def enter_letters_into_sentence(self, word):
+        position_in_array = 0
+        letter_occurences_in_word_counter = self.count_letter_occurences(word)
+        already_entered_letter_counter = 0
+        last_entered_letter_index = 0
 
-        self.used_letters += letter
-        counter = 0
-        counter2 = 0
-        counter3 = 0
-        chwilowe = 0
-
-        #Checking how many times it occurs
-        for m in word:
-            if letter.lower() == m.lower():
-                counter2 +=1
-
-        #Writing letters into the array
-        while counter3 != counter2:
-            for n in word:
-                counter += 1
-                if letter.lower() == n.lower():
-                    counter3 += 1
-                    sentence[counter+chwilowe-1] = letter.lower()
-                    chwilowe += counter
-                    counter = 0
+        while already_entered_letter_counter != letter_occurences_in_word_counter:
+            for letter_of_word in word:
+                position_in_array += 1
+                if self.letter.lower() == letter_of_word.lower():
+                    already_entered_letter_counter += 1
+                    self.sentence[position_in_array + last_entered_letter_index - 1] = self.letter.lower()
+                    last_entered_letter_index += position_in_array
+                    position_in_array = 0
         
-                    
-        print(*sentence)
-        
-                 
+        print(*self.sentence)
 
-game = Game()
-for tries in range(0,10):
-    final_sentence = ""
-    for letter_of_sentence in sentence:
-        final_sentence += u
-    if final_sentence == word:    
-            print(f"You won the game. The word is {word}")
-            break
-    else:
-        game.letter_guess()
+    def play(self, word):
+        self.sentence = self.generate_blanks(word)
+        for tries in range(0, MAX_NUMBER_OF_TRIES):
+            self.request_an_input()
+            self.enter_letters_into_sentence(word) 
+            final_sentence = ""
+            for letter_of_sentence in self.sentence:
+                final_sentence += letter_of_sentence
+            if final_sentence == word:    
+                print(VICTORY_MESSAGE)
+                exit()
 
-print(f"All tries have been used. Game over. The word is {word}")
+        print(GAME_OVER_MESSAGE)    
+
+
+hangman = Hangman()
+hangman.play(WORD)            
+
+
+
 
 
